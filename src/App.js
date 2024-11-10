c4ab076aaf23835ae0e9be862f5eb742b0699eb8
Philip osir Otieno <philiposir2@gmail.com>
2024-11-11 01:19:25 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom'; // Import only necessary components
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import OfferSection from './components/OfferSection';
 import ItemsAll from './components/ItemsAll';
+import FAQsPage from './components/FAQsPage';
+import FeedbackPage from './components/FeedbackPage';
 import TermsAndConditions from './components/pages/TermsAndConditions';
 import PrivacyPolicy from './components/pages/PrivacyPolicy';
 import Contact from './components/pages/Contact';
 import AboutUs from './components/pages/AboutUs';
-import FAQsPage from './components/FAQsPage';
-import FeedbackPage from './components/FeedbackPage';
 import Support from './components/Support';
 import Warranty from './components/Warranty';
 import OrderSupport from './components/OrderSupport';
@@ -32,38 +32,39 @@ const App = () => {
 
   return (
     <div className="App">
-      <Router>
-        <Navbar addToCart={addToCart} cartItems={cart.length} /> {/* Pass cart item count to Navbar */}
-        <CredibilitySection />
-        <main className="main-content">
-          <Routes>
-            {/* Customer Support Routes */}
-            <Route path="/FAQs" element={<FAQsPage />} />
-            <Route path="/Customer-Feedback" element={<FeedbackPage />} />
-            <Route path="/" element={<Support />} />
-            <Route path="/warranty" element={<Warranty />} />
-            <Route path="/order-support" element={<OrderSupport />} />
+      <Navbar addToCart={addToCart} cartItems={cart.length} />
+      <CredibilitySection />
+      
+      {/* Main content */}
+      <main className="main-content">
+        <Routes>
+          {/* Shopping Pages (Route for homepage) */}
+          <Route path="/" element={
+            <div>
+              <OfferSection items={items} />
+              <ItemsAll items={items} addToCart={addToCart} />
+            </div>
+          } />
 
-            {/* Other Pages */}
-            <Route path="/terms" element={<TermsAndConditions />} />
-            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/about" element={<AboutUs />} />
+          {/* Customer Support Routes */}
+          <Route path="/FAQs" element={<FAQsPage />} />
+          <Route path="/Customer-Feedback" element={<FeedbackPage />} />
+          <Route path="/warranty" element={<Warranty />} />
+          <Route path="/order-support" element={<OrderSupport />} />
+          <Route path="/support" element={<Support />} />
 
-            {/* Shopping Pages */}
-            <Route path="/" element={
-              <div>
-                <OfferSection items={items} />
-                <ItemsAll items={items} addToCart={addToCart} />
-              </div>
-            } />
-            
-            {/* Default Route */}
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
-        </main>
-        <Footer />
-      </Router>
+          {/* Other Pages */}
+          <Route path="/terms" element={<TermsAndConditions />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<AboutUs />} />
+
+          {/* Default Route */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </main>
+
+      <Footer />
     </div>
   );
 };
