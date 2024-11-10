1953e32addf91614975ec0ea2ba21aa72fbadb5d
Philip osir Otieno <philiposir2@gmail.com>
2024-11-11 00:34:35 +0300

    updates

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -21,8 +21,8 @@ function ItemCard({ item }){
                     <p>{item.item_features.feature3}</p>
                 </div>
                 <div className="item-price">
-                    <p>kes {item.item_price}</p>
-                    <p><span>kes {item.item_prev_price}</span></p>
+                    <p>Ksh {item.item_price}</p>
+                    <p><span>Ksh {item.item_prev_price}</span></p>
                 </div>
                 <button className="learn-more-btn" onClick={handleLearnMoreClick}>Learn More</button>
                 <button className="add-to-cart-btn">Add to Cart</button>
