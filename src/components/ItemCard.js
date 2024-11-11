e43f9548ad583488697899512000fcbfe3061eb0
Joeken10 <joekentafir@gmail.com>
2024-11-11 13:40:44 +0300

    add

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,35 +1,41 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import './ItemCard.css';
 
-import './ItemCard.css'
+function ItemCard({ item, addToCart }) {
+  const navigate = useNavigate();
 
-function ItemCard({ item }){
-    const navigate = useNavigate();
+  const handleLearnMoreClick = () => {
+    navigate(`/item-details/${item.id}`);
+  };
 
-    const handleLearnMoreClick = () => {
-        navigate(`/item-details/${item.id}`)
-    };
+  const handleAddToCartClick = () => {
+    addToCart(item); // Call addToCart function passed from parent
+  };
 
-    return(
-        <div className="item-card">
-            <div className="item-card-info">
-                <img src={item.item_image_url} alt={item.item_name}/>
-                <h4>{item.item_name}</h4>
-                <div className="item-features">
-                    <p>{item.item_features.feature1}</p>
-                    <p>{item.item_features.feature2}</p>
-                    <p>{item.item_features.feature3}</p>
-                </div>
-                <div className="item-price">
-                    <p>Ksh {item.item_price}</p>
-                    <p><span>Ksh {item.item_prev_price}</span></p>
-                </div>
-                {/* <button className="learn-more-btn" onClick={handleLearnMoreClick}>Learn More</button> */}
-                <button className="add-to-cart-btn">Add to Cart</button>
-            </div>
-            {/* <ItemDetails item={item}/> */}
+  return (
+    <div className="item-card">
+      <div className="item-card-info">
+        <img src={item.item_image_url} alt={item.item_name} />
+        <h4>{item.item_name}</h4>
+        <div className="item-features">
+          <p>{item.item_features.feature1}</p>
+          <p>{item.item_features.feature2}</p>
+          <p>{item.item_features.feature3}</p>
         </div>
-    )
+        <div className="item-price">
+          <p>Ksh {item.item_price}</p>
+          <p><span>Ksh {item.item_prev_price}</span></p>
+        </div>
+        <button className="learn-more-btn" onClick={handleLearnMoreClick}>
+          Learn More
+        </button>
+        <button className="add-to-cart-btn" onClick={handleAddToCartClick}>
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default ItemCard;
