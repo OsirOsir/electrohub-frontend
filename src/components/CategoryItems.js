d2d59e7b20408b406a4e4f3d70d5b9e6580ba576
Philip osir Otieno <philiposir2@gmail.com>
2024-11-12 20:04:05 +0300

    updates

--- src/components/CategoryItems.js
+++ src/components/CategoryItems.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ItemCard from './ItemCard'; // Import the ItemCard component
 import './CategoryItems.css';
 
-const CategoryItems = ({ items }) => {
+const CategoryItems = ({ items, onClose }) => {
     return (
         <div className="category-items">
+            {/* <button className="close-button" onClick={onClose}>Close</button> Close button */}
             <div className="items-grid">
                 {items.map((item) => (
                     <ItemCard key={item.id} item={item} />
