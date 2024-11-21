dadd988eaea0dfbe80c25dbf2ec387f72606ffc2
Philip osir Otieno <philiposir2@gmail.com>
2024-11-21 05:42:10 +0300

    updates

--- src/components/ItemsAll.js
+++ src/components/ItemsAll.js
@@ -6,6 +6,8 @@ import './ItemsAll.css';
 
 function ItemsAll({ items, addToCart }) {
     const navigate = useNavigate();
+    const storedUser = JSON.parse(localStorage.getItem('user')); // Fetch stored user data
+    const userRole = storedUser ? storedUser.role : null; // Determine user role
 
     const handleShowModalsButtons = (e) => {
         e.stopPropagation();
@@ -16,8 +18,12 @@ function ItemsAll({ items, addToCart }) {
         <div className="all-items">
             <div className="all-items-header">
                 <h1>All Items</h1>
-                {/* If user is not admin should not see the below button */}
-                <button className="modify-item-btn" onClick={handleShowModalsButtons}>Modify Items</button> 
+                {/* Conditionally render the button if the user is an admin */}
+                {userRole === "admin" && (
+                    <button className="modify-item-btn" onClick={handleShowModalsButtons}>
+                        Modify Items
+                    </button>
+                )}
             </div>
             
             <div className="items-display">
